1bb2adb4fa24abd64121cf9bd1ad7753418292ca
Cyril Zhang <cyril@mentalcanvas.com>
2014-06-04 10:14:18 -0400

    change Tarjan LCA description

--- java/lapsolver/algorithms/TarjanLCA.java
+++ java/lapsolver/algorithms/TarjanLCA.java
@@ -3,8 +3,8 @@
  * @author Cyril Zhang <cyril.zhang@yale.edu>
  * @date Tue Jun 3 2014
  *
- * An algorithm for calculating the LCA across all off-tree edges in a graph.
- * Complexity: O( (m-n) alpha(n) )
+ * An algorithm for computing offline LCA queries in a tree.
+ * Complexity: amortized O( q alpha(n) )
  */
 
 package lapsolver.algorithms;
