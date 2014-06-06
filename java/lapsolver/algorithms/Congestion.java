e06df93ef1915e6104ff17a12d63e1da4642e0ff
Alex Reinking <alexander.reinking@yale.edu>
2014-06-06 11:37:40 -0400

    Refactored Congestion to be static

--- java/lapsolver/algorithms/Congestion.java
+++ java/lapsolver/algorithms/Congestion.java
@@ -5,10 +5,14 @@
  *
  * For an input graph G and tree T, returns the tree with congestion values on each edge (.getCongestionTree())
  * Complexity: O( M log N )
+ *
+ * Instead of incrementing each vertex of the tree by the value of the path (a,b) in the original graph,
+ * will increment vertex a by weight(a,b), increment vertex b by weight(a,b) and decrement vertex lca(a,b)
+ * by 2 * weight(a,b). Special cases when lca(a,b) = a or b are treated surprisingly smooth.
+ * This will help us have a complexity of O(M) on this segment of the code.
  */
 
 package lapsolver.algorithms;
-
 
 import lapsolver.EdgeList;
 import lapsolver.Graph;
@@ -16,47 +20,23 @@ import lapsolver.Tree;
 import lapsolver.util.TreeUtils;
 
 public class Congestion {
-
-    // Variables needed for the algorithm
-    public Tree spanningTree;
-    public Graph graph;
-    private int vertexCount;
-    private double[] vertexWeights;
-
-    // constructor which performs the computation; retrieve result with computeCongestionTree()
-    public Congestion () { }
-
-    public Tree getCongestionTree(Graph graph, Tree spanningTree) {
-        this.graph = graph;
-        this.spanningTree = spanningTree;
-
-        this.vertexCount = this.graph.nv;
-        this.vertexWeights = computeVertexWeights();
-
-        return computeCongestionTree();
+    public static Tree compute(Graph graph, Tree spanningTree) {
+        double[] vertexWeights = computeVertexWeights(spanningTree, graph);
+        return computeCongestionTree(spanningTree, vertexWeights);
     }
-
-    // For a graph and its tree, gets the lca values for every edge of the graph
-    private int[] computeLCAs() {
-        TarjanLCA lcaSolver = new TarjanLCA(spanningTree);
-        EdgeList edges = new EdgeList(graph);
-
-        return lcaSolver.solve(edges.u, edges.v);
-    }
-
-    // Instead of incrementing each vertex of the tree by the value of the path (a,b) in the original graph,
-    // will increment vertex a by weight(a,b), increment vertex b by weight(a,b) and decrement vertex lca(a,b)
-    // by 2 * weight(a,b). Special cases when lca(a,b) = a or b are treated surprisingly smooth.
-    // This will help us have a complexity of O(M) on this segment of the code.
 
     // Assigns values to vertexWeights
-    private double[] computeVertexWeights() {
-        int[] lcaEdgeValues = computeLCAs();
-        double[] vertexWeights = new double[vertexCount];
+    private static double[] computeVertexWeights(Tree spanningTree, Graph graph) {
+        // Compute the LCAs
+        TarjanLCA lcaSolver = new TarjanLCA(spanningTree);
+        EdgeList edges = new EdgeList(graph);
+        int[] lcaEdgeValues = lcaSolver.solve(edges.u, edges.v);
+
+        double[] vertexWeights = new double[graph.nv];
 
         int index = 0;
 
-        for (int i = 0; i < vertexCount; i++) {
+        for (int i = 0; i < graph.nv; i++) {
             for (int j = 0; j < graph.nbrs[i].length; j++) {
                 if (i < graph.nbrs[i][j]) { // Do only for ordered pairs
                     // A (u,v) edge with LCA and Cost
@@ -78,12 +58,12 @@ public class Congestion {
     }
 
     // Gets the congestion tree
-    private Tree computeCongestionTree() {
-        Tree answer = spanningTree;
+    private static Tree computeCongestionTree(Tree spanningTree, double[] vertexWeights) {
+        Tree answer = spanningTree; // TODO: add clone method to Tree. This is a bug.
 
         int[] bfsOrdering = TreeUtils.bfsOrder(spanningTree);   // Get the BFS ordering
 
-        for (int i = vertexCount - 1; i > 0; i--) {
+        for (int i = spanningTree.nv - 1; i > 0; i--) {
             int child = bfsOrdering[i];
             int parent = spanningTree.getNode(child).getParent().getId();
 
