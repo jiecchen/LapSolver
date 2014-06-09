40587980e4a0861d39a7b719c450554b53b26c5a
Cyril Zhang <cyril.zhang@yale.edu>
2014-06-09 01:46:44 -0400

    fix typos in kelner.m test

--- matlab/tests/kelner.m
+++ matlab/tests/kelner.m
@@ -1,4 +1,4 @@
-function b = kelner( a, x )
+function x = kelner( a, b )
 %KELNER Summary of this function goes here
 %   Detailed explanation goes here
     import lapsolver.lsst.SimulPathTree;
@@ -8,7 +8,7 @@ function b = kelner( a, x )
     solver = KelnerSolver(strat);
     solver.init(a2g(a));
     
-    solver.solve_init;
-    b = solver.solve_return;
+    solver.solve_init(b);
+    x = solver.solve_return;
 end
 
