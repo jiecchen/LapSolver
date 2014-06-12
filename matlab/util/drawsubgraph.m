3fa8c110feb2df62a006c1787c38ed66115bcf38
Cyril Zhang <cyril.zhang@yale.edu>
2014-06-12 12:01:23 -0400

    mark vertices in drawsubgraph

--- matlab/util/drawsubgraph.m
+++ matlab/util/drawsubgraph.m
@@ -1,10 +1,16 @@
-function drawsubgraph(a, s)
+function drawsubgraph(a, s, color)
 %DRAWSUBGRAPH Draws a subgraph.
     xy = specxy(a);
     hold on;
     gplot(a, xy, 'k');
     b = a;
     b(s,s) = 0;
-    gplot(a-b, xy, 'r');
+    
+    if nargin < 3
+        color = 'r';
+    end
+    gplot(a-b, xy, color);
+    scatter(xy(s,1), xy(s,2), color);
+    
     hold off;
 end
