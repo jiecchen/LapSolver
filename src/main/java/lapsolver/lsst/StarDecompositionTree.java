99b7c8fc32ce815520a5f398b4fbeb66c835bd16
Cyril Zhang <cyril.zhang@yale.edu>
2014-06-13 12:08:13 -0400

    add StarDecompositionTree skeleton

---
+++ src/main/java/lapsolver/lsst/StarDecompositionTree.java
@@ -0,0 +1,20 @@
+/**
+ * @file StarDecompositionTree.java
+ * @author Cyril Zhang <cyril.zhang@yale.edu>
+ * @date Fri Jun 13 2014
+ *
+ * An implementation of the star decomposition tree algorithm in EEST05.
+ * Just a skeleton for now.
+ */
+
+package lapsolver.lsst;
+
+import lapsolver.Graph;
+import lapsolver.Tree;
+
+public class StarDecompositionTree {
+    @Override
+    public Tree getTree(Graph graph) {
+        return null;
+    }
+}
