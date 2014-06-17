87252178c5b2c55ece9a1badc86f52d298165a93
Cyril Zhang <cyril.zhang@yale.edu>
2014-06-17 13:20:16 -0400

    add FlatKelnerFlowTree and NesterovSolver (copies of KelnerFlowTree and NesterovSolver for now)

---
+++ src/main/java/lapsolver/solvers/NesterovSolver.java
@@ -0,0 +1,149 @@
+/**
+ * @file NesterovSolver.java
+ * @author Cyril Zhang <cyril.zhang@yale.edu>
+ * @date Tue Jun 17 2014
+ *
+ * An implementation of the primal-dual algorithm of Kelner et al,
+ * with Nesterov acceleration from the Lee-Sidford paper.
+ */
+
+package lapsolver.solvers;
+
+import java.lang.Math;
+import lapsolver.algorithms.DiscreteSampler;
+import lapsolver.algorithms.Stretch;
+import lapsolver.solvers.Solver;
+import lapsolver.Graph;
+import lapsolver.Tree;
+import lapsolver.EdgeList;
+import lapsolver.lsst.SpanningTreeStrategy;
+import lapsolver.solvers.kelner.DirectFlowTree;
+import lapsolver.solvers.kelner.FlowTree;
+import lapsolver.util.TreeUtils;
+import lapsolver.solvers.TreeSolver;
+
+public class NesterovSolver implements Solver {
+    public Tree spanningTree;
+    private SpanningTreeStrategy treeStrategy;
+    private FlowTree flowTree;
+    private TreeSolver treeSolver;
+
+    // edge data to be preprocessed
+    private EdgeList offEdges;
+    private double[] offStretch;
+    private DiscreteSampler edgeSampler;
+
+    // algorithm state
+    private int[] order;
+
+    // initialize solver with a spanning tree strategy
+    public NesterovSolver(SpanningTreeStrategy treeStrategy) {
+        this.treeStrategy = treeStrategy;
+    }
+
+    // initialize solver on a particular graph, and perform preprocessing
+    @Override
+    public void init(Graph graph) {
+        // compute LSST, cache BFS order
+        spanningTree = treeStrategy.getTree(graph);
+        order = TreeUtils.bfsOrder(spanningTree);
+
+        // get off-tree edges, find stretches, initialize sampler
+        offEdges = TreeUtils.getOffTreeEdges(graph, spanningTree);
+        offStretch = Stretch.compute(graph, spanningTree, offEdges).allStretches;
+        edgeSampler = new DiscreteSampler(offStretch);
+
+        // initialize feasible flow finder for spanning tree
+        treeSolver = new TreeSolver();
+        treeSolver.init(spanningTree);
+
+        // initialize the cycle query data structure
+        flowTree = new DirectFlowTree(spanningTree, offEdges);
+    }
+
+    // solve for x in Lx = b, with default parameters
+    @Override
+    public double[] solve(double[] b) {
+        return solve (b, spanningTree.nv + offEdges.ne);
+    }
+
+    // solve for x in Lx = b, with number of iterations
+    public double[] solve(double[] b, int iters) {
+        solve_init(b);
+        for (int i = 0; i < iters; ++i) {
+            solve_iter();
+        }
+        return solve_return();
+    }
+
+    // find feasible flow on LSST
+    // after calling this once, you can call solve_iteration() many times
+    public void solve_init(double[] b) {
+        flowTree.setTreeFlows(treeSolver.solveFlow(b));
+    }
+
+    // improve the current flow on a cycle induced by an off-tree edge
+    public void solve_iter() {
+        if (offEdges.ne == 0) {
+            return;
+        }
+
+        int e = edgeSampler.next();
+        double drop = flowTree.query(e);
+        double resistance = offStretch[e] * offEdges.weight[e];
+
+        flowTree.update(e, -drop / resistance);
+    }
+
+    // return the answer, given the flow state
+    public double[] solve_return() {
+        double[] currentFlow = flowTree.getTreeFlows();
+        double[] voltages = new double[spanningTree.nv];
+
+        // build voltage vector from bottom up
+        for (int i = 1; i < spanningTree.nv; i++) {
+            int v = order[i];
+            int parent = spanningTree.parent[v];
+            double len = spanningTree.weight[v];
+
+            // V = IR
+            voltages[v] = voltages[parent] - currentFlow[v]*len;
+        }
+
+        // subtract mean voltage
+        double meanVoltage = 0;
+        for (int i = 0; i < spanningTree.nv; i++) {
+            meanVoltage += voltages[i];
+        }
+        meanVoltage /= spanningTree.nv;
+        for (int i = 0; i < spanningTree.nv; i++) {
+            voltages[i] -= meanVoltage;
+        }
+
+        return voltages;
+    }
+
+    // get the flow energy (diagnostic)
+    public double getEnergy() {
+        double energy = 0;
+
+        double[] currentFlow = flowTree.getTreeFlows();
+        // sum tree energies
+        for (int i = 0; i < currentFlow.length; i++) {
+            if (i == spanningTree.root) continue;
+
+            double f = currentFlow[i];
+            double r = spanningTree.weight[i];
+            energy += f*f*r;
+        }
+
+        // sum off-tree energies
+        for (int i = 0; i < offEdges.ne; i++) {
+            double f = flowTree.offFlow[i];
+            double r = flowTree.offEdges.weight[i];
+            energy += f*f*r;
+        }
+
+        return energy;
+    }
+}
