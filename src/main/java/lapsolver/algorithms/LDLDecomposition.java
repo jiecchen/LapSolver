7f3e5c194dc1e28b0d05be945d3bed92d237ed25
Serban Stan <serbanandrei.stan@gmail.com>
2014-06-18 16:24:59 -0400

    Fixed a bug when treating special cases in GVM. Updated and corrected LDLDecomposition.

--- src/main/java/lapsolver/algorithms/LDLDecomposition.java
+++ src/main/java/lapsolver/algorithms/LDLDecomposition.java
@@ -18,7 +18,10 @@ import java.util.Arrays;
 public class LDLDecomposition {
     public int N;
     public Graph graph;
+
     public double[] X;
+    public int[] updtDeg;
+
     public int Lindex = 0;
     public int Dindex = 0;
 
@@ -40,6 +43,7 @@ public class LDLDecomposition {
         this.graph = new Graph(G);
         this.N = graph.nv;
         this.X = new double[N];
+        this.updtDeg = new int[N];
 
         if (N != diagValues.length) {
             throw new Error("Graph and addition matrix have different sizes");
@@ -49,40 +53,23 @@ public class LDLDecomposition {
             this.X[i] = diagValues[i];
     }
 
+    EdgeList L;
+    EdgeList D;
+
     public returnPair solve(int numSteps) {
-        EdgeList L = new EdgeList();
-        EdgeList D = new EdgeList();
+        L = new EdgeList();
+        D = new EdgeList();
 
         // Initialize the answer pair
-        initL(L, numSteps);
-        initD(D, numSteps);
+        initL(numSteps);
+        initD(numSteps);
 
-        // I will first construct the L matrix after removing the degree 1 vertices from the graph
-        int[] currentDegree = new int[N];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < graph.deg[i]; j++)
-                X[i] = X[i] + 1 / graph.weights[i][j];
-            currentDegree[i] = graph.deg[i];
-
-            addToEdgeListD(D, i, i, X[i]);
-        }
+        // Update the matrices' diagonal values
+        updateDiag();
 
         for (int i = 0; i < numSteps; i++)
-            if (currentDegree[i] == 1) {                    // Eliminate the degree one vertices
-                for (int j = 0; j < graph.deg[i]; j++) {
-                    int u = i;
-                    int v = graph.nbrs[i][j];
-                    double weight = 1 / graph.weights[i][j];
-
-                    if (u < v) {
-                        addToEdgeListL(L, v, u, -weight / X[u]);
-                        addToEdgeListD(D, v, v, -weight * weight / X[u]);
-
-                        X[v] = X[v] - weight * weight / X[u];
-
-                        currentDegree[v]--;
-                    }
-                }
+            if (updtDeg[i] == 1) {                    // Eliminate the degree one vertices
+                remDeg1(i);
             }
             else {                                          // Eliminate the degree two vertices
                 // Will eliminate a chain starting at 'start' and ending at 'stop'
@@ -108,38 +95,7 @@ public class LDLDecomposition {
                     }
                 }
 
-                double outerRatio = -1;
-                double newEdgeCost = 0;
-                double newValueInLap = 0;
-
-                for (int k = 0; k < 2; k++)
-                    if (graph.nbrs[start][k] == outerStart)
-                        newValueInLap = 1 / graph.weights[start][k];
-
-                for (int u = start; u <= stop; u++) {
-                    for (int k = 0; k < 2; k++) {
-                        int v = graph.nbrs[u][k];
-                        double weight = 1 / graph.weights[u][k];
-
-                        if ((v > u && currentDegree[v] == 2) || (v == outerStop)) { // vertex v is the next vertex in the chain
-                            X[v] = X[v] - weight * weight / X[u];
-
-                            addToEdgeListL(L, v, u, -weight / X[u]);
-                            addToEdgeListD(D, v, v, -weight * weight / X[u]);
-
-                            outerRatio = outerRatio * (weight / X[u]);
-                            newEdgeCost = outerRatio * (-weight);
-
-                            addToEdgeListD(D, outerStart, outerStart, outerRatio * newValueInLap);
-                            newValueInLap = newValueInLap * (weight / X[u]);
-                        }
-                    }
-
-                    addToEdgeListL(L, outerStart, u, outerRatio);
-                }
-
-                addToEdgeListD(D, outerStop, outerStart, -newEdgeCost);
-                addToEdgeListD(D, outerStart, outerStop, -newEdgeCost);
+                removeDeg2Chain(start, stop, outerStart, outerStop);
 
                 i = stop;
             }
@@ -148,51 +104,116 @@ public class LDLDecomposition {
         return answer;
     }
 
-    public void initL(EdgeList el, int steps) {
+    public void removeDeg2Chain(int start, int stop, int outerStart, int outerStop) {
+        // outerValue is used to compute the L values of type (outerStart, u)
+        double outerValue = -1;
+        for (int i = 0; i < 2; i++)
+            if (graph.nbrs[start][i] == outerStart)
+                outerValue = -1 / graph.weights[start][i];
+
+        // newValueInLap is used to mimic the update of the laplacian matrix
+        double newValueInLap = outerValue;
+
+        // Compute the L values adjacent elements in the degree 2 chain
+        for (int u = start; u <= stop; u++) {
+            outerValue = outerValue / X[u];
+
+            X[outerStart] = X[outerStart] - newValueInLap * outerValue;
+
+            addToEdgeListL(outerStart, u, outerValue);
+            addToEdgeListD(outerStart, outerStart, -newValueInLap * outerValue);
+
+            for (int i = 0; i < 2; i++) {
+                int v = graph.nbrs[u][i];
+                double weight = 1 / graph.weights[u][i];
+
+                if (v > u && v != outerStart) {
+                    X[v] = X[v] - weight * weight / X[u];
+                    addToEdgeListL(v, u, -weight / X[u]);
+                    addToEdgeListD(v, v, -weight * weight / X[u]);
+
+                    outerValue = outerValue * weight;
+                    newValueInLap = newValueInLap * (weight / X[u]);
+                }
+            }
+        }
+
+        addToEdgeListD(outerStop, outerStart, newValueInLap);
+        addToEdgeListD(outerStart, outerStop, newValueInLap);
+    }
+
+    public void updateDiag() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < graph.deg[i]; j++)
+                X[i] = X[i] + 1 / graph.weights[i][j];
+            updtDeg[i] = graph.deg[i];
+
+            addToEdgeListD(i, i, X[i]);
+        }
+    }
+
+    public void remDeg1(int k) {
+        for (int i = 0; i < graph.deg[k]; i++) {
+            int u = k;
+            int v = graph.nbrs[k][i];
+            double weight = 1 / graph.weights[k][i];
+
+            if (u < v) {
+                addToEdgeListL(v, u, -weight / X[u]);
+                addToEdgeListD(v, v, -weight * weight / X[u]);
+
+                X[v] = X[v] - weight * weight / X[u];
+
+                updtDeg[v]--;
+            }
+        }
+    }
+
+    public void initL(int steps) {
         // This number of declared elements is an overestimate for the actual needed memory. It has an extra O(steps) field used.
         int cnt = N;
         for (int i = 0; i < steps; i++)
             cnt += graph.deg[i] + 1;
 
-        el.ne = cnt;
-        el.u = new int[cnt];
-        el.v = new int[cnt];
-        el.weight = new double[cnt];
+        L.ne = cnt;
+        L.u = new int[cnt];
+        L.v = new int[cnt];
+        L.weight = new double[cnt];
 
         for (int i = 0; i < N; i++)
-            addToEdgeListL(el, i, i, 1);
+            addToEdgeListL(i, i, 1);
     }
 
-    public void initD(EdgeList el, int steps) {
+    public void initD(int steps) {
         int cnt = N;    // The number of entries on the diagonal
         cnt += 2 * steps;
         for (int i = steps; i < N; i++)
             cnt += 2 * graph.deg[i];    // The number of edges already existing in L
 
-        el.ne = cnt;
-        el.u = new int[cnt];
-        el.v = new int[cnt];
-        el.weight = new double[cnt];
+        D.ne = cnt;
+        D.u = new int[cnt];
+        D.v = new int[cnt];
+        D.weight = new double[cnt];
 
         for (int i = steps; i < N; i++)
             for (int j = 0; j < graph.deg[i]; j++)
                 if (graph.nbrs[i][j] > i) {
-                    addToEdgeListD(el, i, graph.nbrs[i][j], -1 / graph.weights[i][j]);
-                    addToEdgeListD(el, graph.nbrs[i][j], i, -1 / graph.weights[i][j]);
+                    addToEdgeListD(i, graph.nbrs[i][j], -1 / graph.weights[i][j]);
+                    addToEdgeListD(graph.nbrs[i][j], i, -1 / graph.weights[i][j]);
                 }
     }
 
-    public void addToEdgeListL(EdgeList el, int u, int v, double weight) {
-        el.u[Lindex] = u;
-        el.v[Lindex] = v;
-        el.weight[Lindex] = weight;
+    public void addToEdgeListL(int u, int v, double weight) {
+        L.u[Lindex] = u;
+        L.v[Lindex] = v;
+        L.weight[Lindex] = weight;
         Lindex++;
     }
 
-    public void addToEdgeListD(EdgeList el, int u, int v, double weight) {
-        el.u[Dindex] = u;
-        el.v[Dindex] = v;
-        el.weight[Dindex] = weight;
+    public void addToEdgeListD(int u, int v, double weight) {
+        D.u[Dindex] = u;
+        D.v[Dindex] = v;
+        D.weight[Dindex] = weight;
         Dindex++;
     }
 
