0abf0c1f66a5d3293e6672c0d2a9fa826c80c234
Cyril Zhang <cyril.zhang@yale.edu>
2014-06-23 16:45:54 -0400

    swap in fast Kelner data structure again...

--- src/main/java/lapsolver/solvers/KelnerSolver.java
+++ src/main/java/lapsolver/solvers/KelnerSolver.java
@@ -18,6 +18,7 @@ import lapsolver.EdgeList;
 import lapsolver.lsst.SpanningTreeStrategy;
 import lapsolver.solvers.kelner.DirectFlowTree;
 import lapsolver.solvers.kelner.FlowTree;
+import lapsolver.solvers.kelner.KelnerFlowTree;
 import lapsolver.util.TreeUtils;
 import lapsolver.solvers.TreeSolver;
 
@@ -57,7 +58,7 @@ public class KelnerSolver implements Solver {
         treeSolver.init(spanningTree);
 
         // initialize the cycle query data structure
-        flowTree = new DirectFlowTree(spanningTree, offEdges);
+        flowTree = new KelnerFlowTree(spanningTree, offEdges);
     }
 
     // solve for x in Lx = b, with default parameters
