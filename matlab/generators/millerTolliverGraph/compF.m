727cf87cb880f37415a73a58703c3c4119bca8cd
Cyril Zhang <cyril.zhang@yale.edu>
2014-07-15 14:57:09 -0400

    more vectorizing incantations

--- matlab/generators/millerTolliverGraph/compF.m
+++ matlab/generators/millerTolliverGraph/compF.m
@@ -12,19 +12,7 @@ function [f,e] = compF( A, k )
     g = g(:,2:(k+1));
     e = e(2:(k+1));
     
-    % For each i from 1 to k, compute the f(i) based on g(i)
-    d = diag(lap(A));
-    for i = 1:n
-        d(i) = 1 / d(i) ^ 0.5;
-    end
-    sqrtInvD = diag(d);
-    
     % The f values will solve the system D^(1/2) * f = g.
     % f = D^(-1/2) * g
-    
-    f = g;
-    for i = 1:k
-       f(:,i) = sqrtInvD * g(:,i);
-    end
+    f = bsxfun(@times, 1 ./ sqrt(sum(A))', g);
 end
-
