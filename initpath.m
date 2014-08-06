d593364444a880744a34481fab52a9c27083faff
Alex Reinking <alexander.reinking@yale.edu>
2014-08-06 14:51:48 -0400

    initpath now recursively adds directories

--- initpath.m
+++ initpath.m
@@ -5,17 +5,10 @@
 % sets up the correct paths
 
 root = getroot;
-addpath([root, 'matlab']);
-addpath([root, 'matlab/solvers']);
-addpath([root, 'matlab/generators']);
-addpath([root, 'matlab/util']);
-addpath([root, 'matlab/sparsify']);
-addpath([root, 'matlab/cut']);
-addpath([root, 'matlab/solver_tests']);
-addpath([root, 'matlab/viz']);
-addpath([root, 'matlab/lsst']);
-addpath([root, 'matlab/kmp']);
-addpath([root, 'matlab/flow']);
-addpath([root, 'matlab/seg']);
-addpath([root, 'matlab/seg/miller_tolliver']);
-addpath([root, 'matlab/eigvec']);
+
+paths = strsplit(genpath('matlab'), ':');
+paths = paths(1:end-1); % get rid of the terminating ''
+
+for i = 1:length(paths)
+    addpath([root, paths(i)]);
+end
