8f2273b0db9e9ccbce37656ee8f48dbf79c3ef6f
Serban Stan <serbanandrei.stan@gmail.com>
2014-08-05 22:05:44 -0400

    Added a working (?) version of SlowDelayedSimulPathTree. Currently using rate * component size as a comparison measure.

--- src/main/java/lapsolver/lsst/SlowDelayedSimulPathTree.java
+++ src/main/java/lapsolver/lsst/SlowDelayedSimulPathTree.java
@@ -1,10 +1,13 @@
 /**
- * @file SlowDelayedSimulPathTree.java
- * @author Serban Stan <serban.stan@yale.edu>
- * @author Cyril Zhang <cyril.zhang@yale.edu>
- * @date Tue Aug 5 2014
+ * @file SimulPathTree.java
+ * @author Daniel Spielman <spielman@cs.yale.edu>
+ * @date Fri May 30 2014
  *
- * Implements slowing down components in Dan's spanning tree algorithm, slow version.
+ * An attempt at a low-stretch spanning tree, formed by
+ * growing shortest path trees at different rates.
+ *
+ *  javac -classpath ~/rep/YINSmat/java/:. SimulPathTree.java
+ *
  */
 
 package lapsolver.lsst;
@@ -14,16 +17,13 @@ import lapsolver.Graph;
 import lapsolver.Tree;
 import lapsolver.algorithms.UnionFind;
 
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Random;
 import java.util.TreeSet;
 
 public class SlowDelayedSimulPathTree implements SpanningTreeStrategy {
-    // times[node] is time at which node should fire
     public double[] times;
     public double[] rates;
-    // public int[] pArray;
     public int[] ijvI;
     public int[] ijvJ;
     public double[] ijvV;
@@ -34,9 +34,6 @@ public class SlowDelayedSimulPathTree implements SpanningTreeStrategy {
     }
 
     public EdgeList getTreeEdges(Graph graph) {
-//        Logger logger = new Logger();
-//        logger.start("SimulPathTree.log");
-
         ijvI = new int[graph.nv - 1];
         ijvJ = new int[graph.nv - 1];
         ijvV = new double[graph.nv - 1];
@@ -64,14 +61,15 @@ public class SlowDelayedSimulPathTree implements SpanningTreeStrategy {
 
         TreeSet<EdgeEvent> pq = new TreeSet<>(new Comparator<EdgeEvent>() {
             public int compare(EdgeEvent X, EdgeEvent Y) {
-                if (X.time == Y.time) {
-                    if (X.u == Y.u) return Integer.compare(X.v, Y.v);
-                    return Integer.compare(X.u, Y.u);
+                if (X.time * X.number == Y.time * Y.number) {
+                    if (X.time == Y.time) {
+                        if (X.u == Y.u) return Integer.compare(X.v, Y.v);
+                        return Integer.compare(X.u, Y.u);
+                    }
                 }
-                return Double.compare(X.time, Y.time);
+                return Double.compare(X.time * X.number, Y.time * Y.number);
             }
         });
-
 
         for (int u = 0; u < graph.nv; u++)
             for (int i = 0; i < graph.deg[u]; i++) {
@@ -92,8 +90,6 @@ public class SlowDelayedSimulPathTree implements SpanningTreeStrategy {
         int ijvInd = 0;
 
         UnionFind uf = new UnionFind(graph.nv);
-        int[] componentSizes = new int[graph.nv];
-        Arrays.fill(componentSizes, 1);
 
         while (ijvInd < graph.nv - 1) {
             EdgeEvent ev = pq.pollFirst();
@@ -103,34 +99,24 @@ public class SlowDelayedSimulPathTree implements SpanningTreeStrategy {
 
             // if in different comps, add that edge
             if (uf.find(u) != uf.find(v)) {
-                int comp = uf.find(v);
-                componentSizes[comp] += componentSizes[uf.find(u)];
                 uf.union(u, v);
-
-                for (int i = 0; i < graph.ne; i++) {
-                    if (events[i] == null) continue;
-                    if ( (uf.find(events[i].u) == comp) || (uf.find(events[i].v) == comp) ) {
-                        double t = events[i].time + events[i].rate * componentSizes[comp] * events[i].wt;
-                        times[i] = t;
-                        pq.remove(events[i]);
-                        EdgeEvent ev2 = new EdgeEvent(events[i].u, events[i].v, t, events[i].rate, events[i].wt);
-                        pq.add(ev2);
-                        events[i] = ev2;
-                    }
-                }
 
                 ijvI[ijvInd] = u;
                 ijvJ[ijvInd] = v;
                 ijvV[ijvInd] = ev.wt;
                 ijvInd++;
-
             }
+
+            ev.number = 0;
+            for (int i = 0; i < graph.nv; i++)
+                if (uf.find(i) == uf.find(u))
+                    ev.number++;
 
             // for each edge attached to u, see if gives a lower time
             for (int i = 0; i < graph.deg[u]; i++) {
                 int e = edgeNums[u][i];
                 double wt = graph.weights[u][i];
-                double t = ev.time + ev.rate * wt;
+                double t = ev.time + ev.rate * ev.number * wt;
 
                 t += rand.nextDouble() * perturbEpsilon;
 
@@ -151,7 +137,7 @@ public class SlowDelayedSimulPathTree implements SpanningTreeStrategy {
             for (int i = 0; i < graph.deg[v]; i++) {
                 int e = edgeNums[v][i];
                 double wt = graph.weights[v][i];
-                double t = ev.time + ev.rate * wt;
+                double t = ev.time + ev.rate * ev.number * wt;
 
                 t += rand.nextDouble() * perturbEpsilon;
 
@@ -192,6 +178,7 @@ public class SlowDelayedSimulPathTree implements SpanningTreeStrategy {
     public class EdgeEvent {
         int u;
         int v;
+        int number;
         double time;
         double rate;
         double wt;
@@ -199,6 +186,7 @@ public class SlowDelayedSimulPathTree implements SpanningTreeStrategy {
         public EdgeEvent(int u, int v, double time, double rate, double wt) {
             this.u = u;
             this.v = v;
+            this.number = 1;
             this.time = time;
             this.rate = rate;
             this.wt = wt;
