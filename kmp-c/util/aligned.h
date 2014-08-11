4c72c6d3c57b316853671e0255b3fca14931e960
Alex Reinking <alexander.reinking@yale.edu>
2014-08-11 16:57:42 -0400

    Makefile now recursively builds files... don't name any two *.cpp's the same!

--- kmp-c/util/aligned.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "aligned_allocator.h"
-#include "aligned_types.h"
-#include "aligned_opers.h"
--- kmp-c/util/aligned_types.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <vector>
-#include "aligned_allocator.h"
-
-template<typename T> using aligned_vector = std::vector<T, aligned_allocator<T>>;
