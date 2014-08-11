40648051340a9a15b8c622ef9a13839b51be8615
Alex Reinking <alexander.reinking@yale.edu>
2014-08-11 15:59:31 -0400

    Fixed aligned_opers to work with GCC (wtf?)

--- kmp-c/util/aligned_opers.h
+++ kmp-c/util/aligned_opers.h
@@ -3,42 +3,42 @@
 #include "mkl.h"
 
 /* Operator overloads for aligned_vector type */
-inline aligned_vector<float> operator+(aligned_vector<float> x, aligned_vector<float> y)
+inline aligned_vector<float> operator+(const aligned_vector<float> &x, const aligned_vector<float> &y)
 {
     aligned_vector<float> z(x.size());
     vsAdd(x.size(), x.data(), y.data(), z.data());
     return z;
 }
 
-inline aligned_vector<double> operator+(aligned_vector<double> x, aligned_vector<double> y)
+inline aligned_vector<double> operator+(const aligned_vector<double> &x, const aligned_vector<double> &y)
 {
     aligned_vector<double> z(x.size());
     vdAdd(x.size(), x.data(), y.data(), z.data());
     return z;
 }
 
-inline aligned_vector<float> operator-(aligned_vector<float> x, aligned_vector<float> y)
+inline aligned_vector<float> operator-(const aligned_vector<float> &x, const aligned_vector<float> &y)
 {
     aligned_vector<float> z(x.size());
     vsSub(x.size(), x.data(), y.data(), z.data());
     return z;
 }
 
-inline aligned_vector<double> operator-(aligned_vector<double> x, aligned_vector<double> y)
+inline aligned_vector<double> operator-(const aligned_vector<double> &x, const aligned_vector<double> &y)
 {
     aligned_vector<double> z(x.size());
     vdSub(x.size(), x.data(), y.data(), z.data());
     return z;
 }
 
-inline aligned_vector<float> operator*(aligned_vector<float> x, aligned_vector<float> y)
+inline aligned_vector<float> operator*(const aligned_vector<float> &x, const aligned_vector<float> &y)
 {
     aligned_vector<float> z(x.size());
     vsMul(x.size(), x.data(), y.data(), z.data());
     return z;
 }
 
-inline aligned_vector<double> operator*(aligned_vector<double> x, aligned_vector<double> y)
+inline aligned_vector<double> operator*(const aligned_vector<double> &x, const aligned_vector<double> &y)
 {
     aligned_vector<double> z(x.size());
     vdMul(x.size(), x.data(), y.data(), z.data());
