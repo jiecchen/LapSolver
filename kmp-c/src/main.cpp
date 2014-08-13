d97581b3c02f939b80bc14dc3e394ce29153385e
Alex Reinking <alexander.reinking@yale.edu>
2014-08-12 20:51:13 -0400

    Graph converts back to 0-indexing

--- kmp-c/src/main.cpp
+++ kmp-c/src/main.cpp
@@ -14,10 +14,6 @@
 #include "util/aligned.h"
 #include "structures/graph.h"
 
-#define RANDOM ((static_cast<double>(rand())) / RAND_MAX)
-
-int64_t getTimeMS();
-
 int main(int argc, char const *argv[])
 {
     vmlSetMode(VML_EP);
@@ -32,33 +28,5 @@ int main(int argc, char const *argv[])
 
     printGraph(g);
 
-    // const int SIZE = 1000000;
-    // aligned_vector<double> x(SIZE);
-    // aligned_vector<double> y(SIZE);
-    // for (int i = 0; i < SIZE; ++i)
-    // {
-    //     x[i] = RANDOM;
-    //     y[i] = RANDOM;
-    // }
-
-    // int64_t start, stop;
-    // start = getTimeMS();
-    // auto z = x + y;
-    // stop = getTimeMS();
-    // printf("%dms\n", stop - start);
-
     return 0;
-}
-
-int64_t getTimeMS()
-{
-    struct timeval tv;
-
-    gettimeofday(&tv, NULL);
-
-    auto ret = tv.tv_usec;
-    ret /= 1000;
-    ret += (tv.tv_sec * 1000);
-
-    return ret;
 }
--- kmp-c/src/structures/graph.cpp
+++ kmp-c/src/structures/graph.cpp
@@ -5,24 +5,25 @@ Graph::Graph(EdgeList &edges)
 {
     adj.nnz = edges.ne * 2;
     adj.dim = edges.nv;
+
+    // Temp storage. Will be freed when the constructor exits.
     aligned_vector<double> triData(edges.ne);
     aligned_vector<int> triCols(edges.ne);
     aligned_vector<int> triRows(adj.dim);
 
-    int dim = edges.nv;
+    // COO to upper-triangular CSR
     int nnz = edges.ne;
     int info = -1;
     int job [] =
     {
         2,   // COO is converted to the CSR format; the column indices are increasing within each row
-        1,   // zero-based indexing for the matrix in CSR format is used
+        1,   // one-based indexing for the matrix in CSR format is used (required by DSCRADD)
         0,   // zero-based indexing for the matrix in coordinate format is used
         0,   // ???
         nnz, // number of the non-zero elements of the matrix A
         0    // all arrays acsr, ja, ia are filled in for the output storage
     };
-    mkl_dcsrcoo(job,
-                &dim,
+    mkl_dcsrcoo(job, &adj.dim,
                 triData.data(),
                 triCols.data(),
                 triRows.data(),
@@ -32,17 +33,24 @@ Graph::Graph(EdgeList &edges)
                 edges.v.data(),
                 &info);
 
+    // Allocate final memory
     aligned_vector<double> data(adj.nnz);
     aligned_vector<int> cols(adj.nnz);
     aligned_vector<int> rows(adj.dim);
 
+    // B = A + A'
+    int request = 0, sort = 0;
+    double beta = 1.0;
     int WTF_NNZ = adj.nnz + 1; // TODO: wat iz dis, mkl?
-    mkl_dcsradd("t", &(int){0}, &(int){0}, &dim, &dim,
+    mkl_dcsradd("t", &request, &sort, &adj.dim, &adj.dim,
                 triData.data(), triCols.data(), triRows.data(), // A
-                &(double){1.0},
-                triData.data(), triCols.data(), triRows.data(), // bA'
+                &beta, triData.data(), triCols.data(), triRows.data(), // bA'
                 data.data(), cols.data(), rows.data(),
                 &WTF_NNZ, &info);
+
+    // Convert back to 0-based indexing
+    cols.data()[0:adj.nnz]--;
+    rows.data()[0:adj.dim]--;
 
     adj.data = data;
     adj.cols = cols;
