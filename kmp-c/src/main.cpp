2c9a4a5d16f45fb1cee34ce0c63d692c6de398bd
Alex Reinking <alexander.reinking@yale.edu>
2014-08-14 14:54:33 -0400

    Fixing up to work with new Dijkstra interface

--- kmp-c/src/main.cpp
+++ kmp-c/src/main.cpp
@@ -4,31 +4,34 @@
  * Example benchmark for vector arithmetic
  */
 #include <cstdlib>
+#include <string>
 #include "structures/GraphFactory.h"
 #include "algorithms/ShortestPathTree.h"
 
 int main(int argc, char const *argv[])
 {
     Graph g;
+    std::string inFileName;
     try
     {
         if (argc < 2)
-            g = GraphFactory::fromFile("inG.ijv");
+            inFileName = "inG.ijv";
         else
-            g = GraphFactory::fromFile(argv[1]);
+            inFileName = argv[1];
+        g = GraphFactory::fromFile(inFileName);
     }
     catch (int e)
     {
         if (e > 0)
-            fprintf(stderr, "Failed to parse input file (line %d)\n", e + 1);
+            fprintf(stderr, "Parse error in '%s' on line %d\n", inFileName.c_str(), e + 1);
         else
-            fprintf(stderr, "Failed to open file\n");
+            fprintf(stderr, "Failed to open file '%s'\n", inFileName.c_str());
         return 0;
     }
 
     ShortestPathTree spt(g, 0);
     for (int i = 0; i < g.nv; ++i)
-        printf("%d ", spt.getParentArray()[i]);
+        printf("%d ", spt.parent[i]);
     printf("\n");
 
     return 0;
