6ce9b3e13325d94ea785dd4fb1d42226e2406b4a
Alex Reinking <alexander.reinking@yale.edu>
2014-08-15 12:14:42 -0400

    Test system improvements

--- kmp-c/test/runTest.sh
+++ kmp-c/test/runTest.sh
@@ -1,8 +1,8 @@
 cd $1
 # Set up library paths
 source /opt/intel/composerxe/bin/compilervars.sh intel64
-export DYLD_LIBRARY_PATH=../:$DYLD_LIBRARY_PATH
-export LD_LIBRARY_PATH=../:$LD_LIBRARY_PATH
+export DYLD_LIBRARY_PATH=../../:$DYLD_LIBRARY_PATH
+export LD_LIBRARY_PATH=../../:$LD_LIBRARY_PATH
 # Run the test
 if [ "$TESTMODE" = "auto" ]; then
 	./$1 input | cmp - output
