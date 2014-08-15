1427c68fd88a1bf5dfda142a6736c48855737c56
Cyril Zhang <cyril.zhang@yale.edu>
2014-08-15 12:10:39 -0400

    add Cholesky order test

---
+++ kmp-c/test/cholesky_order/test.cpp
@@ -0,0 +1,34 @@
+// Test for Cholesky elimination order
+
+#include <cstdlib>
+#include <string>
+#include <structures/Graph.h>
+#include <structures/GraphLoader.h>
+#include <algorithms/PartialCholeskyOrder.h>
+
+int main(int argc, char const *argv[])
+{
+    Graph g;
+    std::string inFileName;
+    try
+    {
+        if (argc < 2)
+            inFileName = "inG.ijv";
+        else
+            inFileName = argv[1];
+        g = GraphLoader::fromFile(inFileName);
+    }
+    catch (int e)
+    {
+        if (e > 0)
+            fprintf(stderr, "Parse error in '%s' on line %d\n", inFileName.c_str(), e + 1);
+        else
+            fprintf(stderr, "Failed to open file '%s'\n", inFileName.c_str());
+        return 0;
+    }
+
+    PartialCholeskyOrder gvr(g);
+    printf("%d\n", gvr.removed); // 18
+
+    return 0;
+}
